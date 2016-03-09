3e46e096a518187899173c067d11d292c267a2e5
msandim <migsandim@hotmail.com>
2016-03-09 03:09:39 +0000

    ta

---
+++ IEEE Data Mining/sessao3.R
@@ -0,0 +1,14 @@
+library(ISLR)
+library(boot)
+
+set.seed(17)
+
+cv.error.10 <- rep(0,10)
+
+for (i in 1:10) {
+  glm.fit=glm(mpg ~ poly(horsepower ,i),data=Auto)
+  cv.error.10[i]=cv.glm(Auto,glm.fit,K=10)$delta[1]
+}
+
+print(cv.error.10)
+plot(cv.error.10)
